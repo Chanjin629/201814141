431a1f28678645bf176e6fa89c71ea3031222933
Kim, Chanjin <ihercwdxz@gmail.com>
2022-09-26 15:51:25 +0900

    Main.java 수정#2

--- src/d201814141/Main.java
+++ src/d201814141/Main.java
@@ -2,9 +2,12 @@ package d201814141;
 
 public class Main {
 
+	static void say(String s){
+		System.out.prinln(s);
+	}
+
 	public static void main(String[] args) {
-		String s = "hello world";
-		System.out.println(s);
+		say("hello world");
 
 	}
 
